bbe19cf6e920cc5551363fa5d72d1638c96c9c70
Anvarov Doniyor <a.doniyor@aliftech.uz>
2022-08-27 15:57:51 +0500

    fic

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thingengineer/mysqli-database-class',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '16564ba0717fad64617a1ce4a8d3f9b90e68c7c2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'thingengineer/mysqli-database-class' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '16564ba0717fad64617a1ce4a8d3f9b90e68c7c2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
